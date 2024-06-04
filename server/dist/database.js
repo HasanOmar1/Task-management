896d3e12716fec9e4fd15bc00c21d2fc5bdc010c
HasanOmar1 <r3domar1@gmail.com>
2024-06-04 14:26:28 +0300

    updated build command

--- server/dist/database.js
+++ server/dist/database.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "taskmanager",
+    database: process.env.MYSQL_DB_NAME,
 });
 db.connect((err) => {
     if (err)
